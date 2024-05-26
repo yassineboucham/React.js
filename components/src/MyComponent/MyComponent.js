c4a2a61da7035f806beefad670e00dc42ffbad41
yassineboucham <127396067+yassineboucham@users.noreply.github.com>
2024-05-26 14:59:38 +0100

    creat commponent

---
+++ components/src/MyComponent/MyComponent.js
@@ -0,0 +1,8 @@
+export default function MyComponent() {
+    return (
+        <div>
+        <h1>Hello, this is my component!</h1>
+        <p>Welcome to my React application.</p>
+      </div>
+    );
+}
