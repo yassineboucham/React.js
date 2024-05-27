4a3e0dd4d16b0248a0d4dc912b476ed25adb8f77
yassineboucham <127396067+yassineboucham@users.noreply.github.com>
2024-05-28 00:14:57 +0100

    is and else

--- components/src/Component/props_children.js
+++ components/src/Component/props_children.js
@@ -1,6 +1,14 @@
 export default function Form(props) {
+    const y = 10;
+    let mess;
+    if (y > 18){
+        mess = <h3>adult</h3>
+    } else {
+        mess = <h3>kids</h3>
+    }
     return (
         <>
+            {mess}
             <label>{props.Label_Name}</label>
             <input name={props.value} value={props.value} type="text"/><br/>
             <p>{props.children}</p>
